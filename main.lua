83c62d5ec6f3e99cbb437bdcdc845adc88e1a3c7
cshannon0128 <cshannon0128@gmail.com>
2018-06-07 09:43:02 -0700

    Prints money to screen and text changes position to adpat for digit increase

--- main.lua
+++ main.lua
@@ -101,8 +101,9 @@ function love.draw()
     love.graphics.print(player_money:get_current_money(), 270, 63, r, sx, sy, ox, oy, kx, ky)
     love.graphics.print(enemy_money:get_current_money(), screen_width - enemy_font_x, 63, r, sx, sy, ox, oy, kx, ky)
 
-    if enemy_money:get_current_money() >= 100 then
-      enemy_font_x = enemy_font_x + 64
+    if enemy_money:get_current_money() >= 100 and has_gone_up == false then
+      enemy_font_x = enemy_font_x + 40
+      has_gone_up = true
     end
   end
 
