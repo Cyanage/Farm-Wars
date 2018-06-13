06e2d97e41845ce5947df0a3d93a1496bd0f9995
EarthenSkyLaptop <earthenskymath@gmail.com>
2018-06-13 09:59:24 -0700

    Revert "Revert "Merge branch 'master' of https://github.com/Cyanage/Farm-Wars""
    This reverts commit 6626d9757110c9f00e2cfbb72586ad178ef55959.

--- src/objects/money.lua
+++ src/objects/money.lua
@@ -5,7 +5,7 @@ function Money:new()
     -- Add member variables here.
     selfObj = {
         total_money = 50,
-        money_per_turn = 0,
+        money_per_turn = 1,
         time = 0,
         button_clicked = false
     }
