3ceae819eb6e160c90715a390b3e7a04e46a4f76
cshannon0128 <cshannon0128@gmail.com>
2018-06-18 10:14:13 -0700

    Farmer animation added

--- src/objects/farmer.lua
+++ src/objects/farmer.lua
@@ -2,10 +2,16 @@ Farmer = {}
 
 -- This function makes a new instance of this object.  (this is the constructor.)
 function Farmer:new(position)
+
+    self.farmer_sprites = {love.graphics.newImage("resc/images/farmer/farmer1.png"),
+    love.graphics.newImage("resc/images/farmer/farmer2.png"),
+    love.graphics.newImage("resc/images/farmer/farmer3.png"),
+    love.graphics.newImage("resc/images/farmer/farmer4.png")
+  }
     -- Add member variables here.
     selfObj = {
         m_position = position,
-    }
+      }
 
     -- Make this a class.
     self.__index = self
@@ -21,8 +27,9 @@ function Farmer:moveForwards()
 
 end
 
-function Farmer:draw()
+function Farmer:draw(i)
 
+      love.graphics.draw(self.farmer_sprites[i], 0, 0, r, sx, sy, ox, oy, kx, ky)
 end
 
-return Money
+return Farmer
